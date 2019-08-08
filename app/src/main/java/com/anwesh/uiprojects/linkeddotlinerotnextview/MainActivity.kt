0a67df9bcba12ba5ce87996d6e24ee7b8eb516d8
Anwesh43 <anweshthecool0@gmail.com>
2019-08-08 11:50:10 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddotlinerotnextview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddotlinerotnextview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddotlinerotnextview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.dotlinerotnextview.DotLineRotNextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DotLineRotNextView.create(this)
     }
 }
