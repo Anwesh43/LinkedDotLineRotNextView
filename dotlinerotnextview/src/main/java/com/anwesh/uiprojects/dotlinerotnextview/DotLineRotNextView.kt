d59e48161b0b139e564d04ce2f7263b4a90f1f05
Anwesh43 <anweshthecool0@gmail.com>
2019-08-08 11:24:14 +0530

    adding DotLineRotNextView class

--- dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
+++ dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
@@ -46,3 +46,21 @@ fun Canvas.drawDLRNode(i : Int, scale : Float, ballDraw : Boolean, paint : Paint
     restore()
 }
 
+class DotLineRotNextView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
