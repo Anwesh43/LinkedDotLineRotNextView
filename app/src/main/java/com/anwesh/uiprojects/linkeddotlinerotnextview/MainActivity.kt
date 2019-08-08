0f23563b1e3459263e013effb639a2280434a0df
Anwesh43 <anweshthecool0@gmail.com>
2019-08-08 11:51:13 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkeddotlinerotnextview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddotlinerotnextview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkeddotlinerotnextview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.dotlinerotnextview.DotLineRotNextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DotLineRotNextView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
