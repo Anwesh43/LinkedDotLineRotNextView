3343f9152d6001ec7008aed208f69494a1f396cd
Anwesh43 <anweshthecool0@gmail.com>
2019-08-08 11:20:00 +0530

    adding drawDLRNode canvas extension method

--- dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
+++ dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
@@ -18,3 +18,31 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#311B92")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val scGap : Float = 0.05f
+val rotDeg : Float = 180f
+val rFactor : Float = 3.1f
+
+fun Canvas.drawDotLine(size : Float, scale : Float, ballDraw : Boolean, paint : Paint) {
+    save()
+    rotate(rotDeg * scale)
+    drawLine(0f, 0f, -size, 0f, paint)
+    if (ballDraw) {
+        drawCircle(0f, -size, size / rFactor, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawDLRNode(i : Int, scale : Float, ballDraw : Boolean, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawDotLine(size, scale, ballDraw, paint)
+    restore()
+}
+
