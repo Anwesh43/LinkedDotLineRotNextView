885fda953dc281de57f151f74842e1a38bb0b232
Anwesh43 <anweshthecool0@gmail.com>
2019-08-08 11:10:38 +0530

    adding libraries and constants

--- dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
+++ dotlinerotnextview/src/main/java/com/anwesh/uiprojects/dotlinerotnextview/DotLineRotNextView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.dotlinerotnextview
 /**
  * Created by anweshmishra on 08/08/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val sizeFactor : Float = 2f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.05f
